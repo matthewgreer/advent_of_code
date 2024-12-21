a7ffdb60aec9b4de219122c64d37a4ed72715614
matthewgreer <matthew.greer.dev@gmail.com>
2024-12-21 16:32:47 -0500

    actually solved pt2, day12

--- day12.js
+++ day12.js
@@ -56,12 +56,12 @@ class Plot {
   }
 
   checkBorders() {
-    // returns whether the plots to the left, up, right, and down are different crops (or edges of the map)
+    // returns whether the plots to the West, North, East, and South are different crops (or edges of the map)
     let borders = {
-      "left": this.col > 0 ? this.map[this.row][this.col - 1] !== this.crop : true,
-      "up": this.row > 0 ? this.map[this.row - 1][this.col] !== this.crop : true,
-      "right": this.col < this.map[0].length - 1 ? this.map[this.row][this.col + 1] !== this.crop : true,
-      "down": this.row < this.map.length - 1 ? this.map[this.row + 1][this.col] !== this.crop : true
+      "west": this.col > 0 ? this.map[this.row][this.col - 1] !== this.crop : true,
+      "north": this.row > 0 ? this.map[this.row - 1][this.col] !== this.crop : true,
+      "east": this.col < this.map[0].length - 1 ? this.map[this.row][this.col + 1] !== this.crop : true,
+      "south": this.row < this.map.length - 1 ? this.map[this.row + 1][this.col] !== this.crop : true
     }
 
     return borders;
@@ -80,16 +80,39 @@ class Plot {
 class Region {
   constructor(id) {
     this.id = id,
-      this.plots = []
+    this.plots = [],
+    this.borders = {
+      "west": {},
+      "north": {},
+      "east": {},
+      "south": {}
+    }
   }
 
   addPlot(plot) {
     this.plots.push(plot);
+    this.sortPlotBorders(plot);
   }
 
   getPlot(row, col) {
     const plot = this.plots.find(plot => plot.row === row && plot.col === col);
     return plot;
+  }
+
+  sortPlotBorders(plot) {
+    for (const [direction, hasBorder] of Object.entries(plot.borders)) {
+      if (hasBorder) this.addToBorderList(direction, plot.row, plot.col);
+    }
+  }
+
+  addToBorderList(dir, row, col) {
+    if (dir === "west" || dir === "east") {
+      if (this.borders[dir][col] === undefined) this.borders[dir][col] = [];
+      this.borders[dir][col].push(row);
+    } else {
+      if (this.borders[dir][row] === undefined) this.borders[dir][row] = [];
+      this.borders[dir][row].push(col);
+    }
   }
 
   get area() {
@@ -100,140 +123,54 @@ class Region {
     return this.plots.reduce((total, plot) => total += plot.perimeter, 0);
   }
 
-  sides() {
-    // pathfind around the perimeter of the region, counting sides at changes in direction
-
-    const directions = ["up", "right", "down", "left"];
-    const vectors = {
-      "up": [-1, 0],
-      "right": [0, 1],
-      "down": [1, 0],
-      "left": [0, -1]
-    };
-    let sides = 0;
-    const startingPlot = this.plots[0];
-
-    // console.log("region id", this.id);
-
-    // console.log("startingPlot", startingPlot.row, startingPlot.col);
-
-    const startingDirection = 1 // meaning "right"
-    // const visited = [] // stack for `row, col` key <--- maybe... is this necessary? backtracking?
-
-    let currentPlot = startingPlot;
-    let travellingDirection = startingDirection
-    let outsideDirection = 0 // meaning "up"
-    let started = false;
-
-    const moveToNextPlot = (plot) => {
-
-      // console.log("calling moveToNextPlot(", plot.row, plot.col, ")")
-
-      let row = plot.row,
-          col = plot.col,
-          [vr, vc] = vectors[directions[travellingDirection]];
-      // visited.push(`${row},${col}`); // ??? idk if necessary
-      // move to the next plot
-      const nextPlot = this.getPlot(row + vr, col + vc);
-
-      // console.log("nextPlot is", nextPlot.row, nextPlot.col, "crop", nextPlot.crop);
-
-      // TODO: WHAT IF IT ISN'T FOUND??
-      return nextPlot
-    };
-
-    const turn = (direction) => {
-      // THE LOGIC FOR COUNTING SIDES ISN'T YET RIGHT.
-      // console.log("---");
-      // console.log("recording that side")
-
-      sides ++;
-
-      // console.log("sides", sides);
-      // console.log("turning", direction);
-
-      const turn = direction === "right" ? 1 : -1;
-
-      // console.log("-->  travelling", travellingDirection, "+", turn, "=", (4 + travellingDirection + turn) % 4);
-      // console.log("-->  outside", outsideDirection, "+", turn, "=", (4 + outsideDirection + turn) % 4);
-
-      const newTravellingDirection = ((4 + travellingDirection + turn) % 4);
-      const newOutsideDirection = ((4 + outsideDirection + turn) % 4);
-
-      // console.log("travellingDirection changed from", directions[travellingDirection], "to", directions[newTravellingDirection]);
-      // console.log("outside direction changed from", directions[outsideDirection], "to", directions[newOutsideDirection]);
-
-      travellingDirection = newTravellingDirection;
-      outsideDirection = newOutsideDirection;
-    }
-    // start with the first plot in this.plots. the first plot of a Region must perforce have a border on the left and the top. we'll start there and move right along the top side.
-
-    while (!started || !(currentPlot === startingPlot && travellingDirection === startingDirection)) {
-      started = true;
-
-      // console.log("===");
-      // console.log("BEGINNING OF LOOP");
-      // console.log("currentPlot", currentPlot.row, currentPlot.col, "crop", currentPlot.crop, ", facing", directions[travellingDirection]);
-      // console.log("plot borders", currentPlot.borders);
-      // console.log("checking outside (", directions[outsideDirection], ")...")
-
-      if (currentPlot.borders[directions[outsideDirection]]){
-        // block outside, try ahead
-
-        // console.log("border outside (", directions[outsideDirection],")! try to move ahead -->", directions[travellingDirection])
-
-        if (!currentPlot.borders[directions[travellingDirection]]) {
-
-          // console.log("no border ahead! stepping forward", directions[travellingDirection]);
-
-          let nextPlot = moveToNextPlot(currentPlot);
-
-          // console.log("setting currentPlot", currentPlot.row, currentPlot.col, "to nextPlot", nextPlot.row, nextPlot.col);
-
-          currentPlot = nextPlot;
-
-        } else {
-          // block outside and ahead, turn inside
-
-          // console.log("block ahead! end of side.")
-          turn("right")
-        }
-      } else {
-        // no block outside, turn outside
-        turn("left");
-
-        let nextPlot = moveToNextPlot(currentPlot);
-
-        // console.log("setting currentPlot", currentPlot.row, currentPlot.col, "to nextPlot", nextPlot.row, nextPlot.col);
-
-        currentPlot = nextPlot;
-      }
-      // console.log("should be looping because started is", started, "but currentPlot === startingPlot && travellingDirection === startingDirection is still false ->", !(currentPlot === startingPlot && travellingDirection === startingDirection));
-    }
-
-    // console.log("We've come back to the start.")
-    return sides;
-
+  countSides() {
+    // count the borders of each of the plots of the region, then filter out the continuous ones
     /*
-      0 1 2 3 4 5 6 7 8           0 1 2 3 4 5 6 7 8
-    0             C C                         - +
-    1             C > C                         +
-    2           C C
-    3       C C C
-    4         C     C
-    5         C C
-    6           C
-
-    side 1: 0,6 -> 0,7 top        side  8: 2,5 -> 3,5 right     side 15: 5,5 -> 5,4 bottom    side 22: 2,6 -> 0,6 left
-    side 2: 0,7 -> 1,7 right      side  9: 3,5 -> 3,4 bottom    side 16: 5,4 -> 3,4 left      STOP: 0,6 top
-    side 3: 1,7 -> 1,8 top        side 10: 3,4 -> 5,4 right     side 17: 3,4 -> 3,3 bottom
-    side 4: 1,8 right             side 11: 5,4 -> 5,5 top       side 18: 3,3 left
-    side 5: 1,8 -> 1,6 bottom     side 12: 5,5 -> 6,5 right     side 19: 3,3 -> 3,5 top
-    side 6: 1,6 -> 2,6 right      side 13: 6,5 bottom           side 20: 3,5 -> 2,5 left
-    side 7: 2,6 -> 2,5 bottom     side 14: 6,5 -> 5,5 left      side 21: 2,5 -> 2,6 top
-
-    Ok at 1,7 facing right we check up and there's no border so we count a side and turn back up. so does a left turn (to the outside) need to be a turn without incrementing sides? should it step forward first?
+    this.borders = {
+      "west": {
+        "0": [1, 2, 6]    <--  key: col, val: [row, row, row]
+      },
+      "north": {
+        "4": [3, 4, 5, 7] <--  key: row, val: [col, col, col]
+      },
+      etc...
+    }
     */
+
+    const calculateSidesFromBorderList = (list) => {
+      list.sort((a, b) => a - b);
+      const processedList = [];
+
+      while (true) {
+        const currentBorder = list.shift();
+        if (currentBorder === undefined) break;
+
+        const previousBorder = processedList.at(-1);
+
+        if (previousBorder === undefined) {
+          processedList.push(currentBorder);
+          continue;
+        }
+
+        if (currentBorder - previousBorder === 1) {
+          processedList.pop(); // removing continuous side segment
+        }
+
+        processedList.push(currentBorder);
+      }
+
+      return processedList.length;
+    }
+
+    let sides = 0;
+
+    for (const borders of Object.values(this.borders)) {
+      for (const list of Object.values(borders)) {
+        sides += calculateSidesFromBorderList(list);
+      }
+    }
+
+    return sides;
   }
 }
 
@@ -264,7 +201,7 @@ class Crop {
 class Farm {
   constructor(array) {
     this.map = array,
-      this.rows = array.length;
+    this.rows = array.length;
     this.columns = array[0].length;
     this.crops = [],
       this.coordTable = new Map();
@@ -309,27 +246,27 @@ class Farm {
   }
 
   chooseRegionForPlot(plot) {
-    let up, upReg, left, leftReg, region;
+    let north, northReg, west, westReg, region;
 
     if (plot.row > 0) {
-      up = this.getCoordPlot(plot.row - 1, plot.col);
-      upReg = this.getCoordRegion(up.row, up.col);
+      north = this.getCoordPlot(plot.row - 1, plot.col);
+      northReg = this.getCoordRegion(north.row, north.col);
     }
     if (plot.col > 0) {
-      left = this.getCoordPlot(plot.row, plot.col - 1);
-      leftReg = this.getCoordRegion(left.row, left.col);
+      west = this.getCoordPlot(plot.row, plot.col - 1);
+      westReg = this.getCoordRegion(west.row, west.col);
     }
 
-    if (up &&
-      up.crop === plot.crop &&
-      left &&
-      left.crop === plot.crop &&
-      upReg !== leftReg) {
-      region = this.mergeRegions(plot.crop, upReg, leftReg);
-    } else if (up && up.crop === plot.crop) {
-      region = upReg;
-    } else if (left && left.crop === plot.crop) {
-      region = leftReg;
+    if (north &&
+      north.crop === plot.crop &&
+      west &&
+      west.crop === plot.crop &&
+      northReg !== westReg) {
+      region = this.mergeRegions(plot.crop, northReg, westReg);
+    } else if (north && north.crop === plot.crop) {
+      region = northReg;
+    } else if (west && west.crop === plot.crop) {
+      region = westReg;
     } else {
       const crop = this.getCrop(plot.crop);
       region = crop.addRegion()
@@ -364,7 +301,7 @@ class Farm {
           this.addCrop(cropSymbol);
         }
         const crop = this.getCrop(cropSymbol);
-        const region = !crop.regions.length ? crop.addRegion() : this.chooseRegionForPlot(plot);
+        const region = crop.regions.length === 0 ? crop.addRegion() : this.chooseRegionForPlot(plot);
         region.addPlot(plot);
         this.saveCoordRegion(row, col, cropSymbol, region.id);
       }
@@ -384,7 +321,7 @@ class Farm {
   calculateFencingPriceBySides() {
     const price = this.crops.reduce((farmTotal, crop) => {
       return farmTotal += crop.regions.reduce((cropTotal, region) => {
-        return cropTotal += region.area * region.sides();
+        return cropTotal += region.area * region.countSides();
       }, 0);
     }, 0);
 
@@ -394,34 +331,15 @@ class Farm {
 
 
 // const inputString = "AAAA\nBBCD\nBBCC\nEEEC";
-const inputString = "AAAAAA\nAAABBA\nAAABBA\nABBAAA\nABBAAA\nAAAAAA"
-
+// const inputString = "AAAAAA\nAAABBA\nAAABBA\nABBAAA\nABBAAA\nAAAAAA"
 // const inputString = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE\n";
-// const inputString = input;
+
+const inputString = input;
 
 const farmArray = farm(inputString);
 const myStupidFarm = new Farm(farmArray);
 
 myStupidFarm.plotMap();
-myStupidFarm.crops.forEach(c => {
-  c.regions.forEach(r => {
-    console.log("crop", c.symbol, "id", r.id, "sides", r.sides());
-  });
-});
 
-// const cCrop = myStupidFarm.getCrop("C");
-
-// const r = cCrop.regions[0];
-
-// cCrop.regions.forEach(r => {
-  // const sides = r.sides(),
-  //       area = r.area;
-  // console.log("")
-  // console.log("REGION ID:", r.id, "HAS", sides, "SIDES!");
-  // console.log("AND AN AREA OF", area);
-  // console.log("SO FENCING COSTS", sides * area)
-  // console.log("")
-// })
-
-console.log(myStupidFarm.calculateFencingPriceByPerimeter());
-console.log(myStupidFarm.calculateFencingPriceBySides());
+console.log(myStupidFarm.calculateFencingPriceByPerimeter());   // 1319878
+console.log(myStupidFarm.calculateFencingPriceBySides());       // 784982
